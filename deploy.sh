b10794c502f9756fa69ccc8f011d49aacb85e7b3
Agustin Polo <poloagustin@gmail.com>
2016-09-28 01:02:34 -0300

    cambiar variable

--- deploy.sh
+++ deploy.sh
@@ -26,10 +26,11 @@ if [ "$TRAVIS_BRANCH" == "$SOURCE_BRANCH_STAGING" ]; then
   git clone $SSH_TARGET_REPO_STAGING out
   cd out
   git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+  git rm . || exit 0
+
   cd ..
 
   # Clean out existing contents
-  rm -rf out/**/* || exit 0
 
   # Run our compile script
   npm run deploy-staging
