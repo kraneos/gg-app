6c5a5301f78cc00265d3cac1d1243b8cdd8c0c15
apolo_codes <poloagustin@gmail.com>
2016-10-01 16:21:51 -0300

    hacer script para deployar a staging. windows-version

--- deploy.staging.bat
+++ deploy.staging.bat
@@ -1,6 +1,17 @@
-git clone https://github.com/kraneos/gg-app-staging
-cd gg-app-staging
-git checkout staging
-git rm -r **/*.*
+set mypath=%cd%
 cd ..
-ng build
+call git clone https://github.com/kraneos/gg-app-staging
+cd gg-app-staging
+call git checkout gh-pages
+call git rm . -r
+cd %mypath%
+call npm run deploy-staging
+move .\dist\*.* ..\gg-app-staging
+rd /s dist
+cd ..\gg-app-staging
+call git add .
+call git commit . -m "deploy a staging."
+call git push origin gh-pages
+cd ..
+rd /s gg-app-staging
+cd %mypath%
